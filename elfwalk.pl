cc252740eab8075255417d72b05e5b578d8a0baf
Xavier G. <xavier.elfwalk@kindwolf.org>
2015-06-17 18:35:02 +0200

    Add README and script skeleton.

---
+++ elfwalk.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+#A simple toy script displaying the contents of an ELF file.
+
+# (c) 2015 - Xavier G.
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+
+
+exit(0);
